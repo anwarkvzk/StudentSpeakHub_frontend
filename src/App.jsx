0c5af6c10e33e47a3df0cbccc69ee877a06923c4
anwarkvzk <anvarvzk521@gmail.com>
2024-05-01 16:28:18 +0530

    Routes created

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Project from "./pages/Project";
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Project/>}/> 
+      </Routes>
+    </BrowserRouter>
+  );
+}
