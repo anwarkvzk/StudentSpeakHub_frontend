748326a8cc65e7c9d6fb776a9b29278650fd2786
anwarkvzk <anvarvzk521@gmail.com>
2024-05-14 21:26:38 +0530

    complete about and projects pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font-semibold text-center my-7">About Student Speak Hub</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              At Student Speak Hub, we curate a diverse collection of articles
+              and blogs covering an array of topics tailored to student life.
+              Dive into our repository of insights, where you'll find narratives
+              on navigating university experiences, maintaining a balance
+              between academics and family life, embarking on transformative
+              study abroad adventures, exploring career options, and nurturing
+              personal growth. Our platform aims to foster a supportive
+              community where students can share their stories, challenges, and
+              triumphs, empowering one another on their academic and personal
+              journeys. Join us as we embark on this collective exploration of
+              student life and beyond
+            </p>
+            <p>
+              As an inclusive space for student voices, Student Speak Hub
+              strives to offer a platform that celebrates the richness of
+              diverse experiences and perspectives. Whether you're seeking
+              practical advice, inspiration, or simply a sense of connection
+              with fellow students, our platform is designed to provide valuable
+              insights and foster meaningful conversations. We believe that by
+              sharing our stories and supporting each other, we can create a
+              more inclusive and supportive community where every voice is heard
+              and valued. Join us in shaping the narrative of student life and
+              together, let's inspire, learn, and grow.
+            </p>
+            <p>
+              With a commitment to transparency and authenticity, Student Speak
+              Hub is driven by the belief that every student has a story worth
+              sharing. Our platform encourages open dialogue and exchange of
+              ideas, creating a safe space for individuals to express themselves
+              freely. Whether you're a seasoned writer or new to sharing your
+              thoughts publicly, we welcome you to join our community and
+              contribute your unique perspective. Together, we can build a
+              vibrant hub of knowledge, inspiration, and support, empowering
+              students to navigate the complexities of university life with
+              confidence and resilience.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
